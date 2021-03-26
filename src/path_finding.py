fb2ec5d8440d9c8412d9baf5918a01f34a1a60ed
Al Colon <alcolon@brandeis.edu>
2021-03-26 12:14:27 -0400

    set up for path finding algorithm

---
+++ src/path_finding.py
@@ -0,0 +1,46 @@
+progress_stack = []
+global fiduciary  #dictionary
+#initialize elsewhere
+global num_of_fid #set amount of fiducial points
+global same
+
+def setWorld()
+    same = true
+    num_of_fid = 10
+
+def
+    use computer vision to detect fiducials
+    transform to that area
+    use this information to figure out place
+
+def where_am_i
+    using the information of the fiducial to indicate place
+    need to make a guess intital guess
+    stack for path Finding
+
+def journey #how many fiducials were encountered already
+    return stack.size/num_of_fid
+
+
+def starting
+    if stack is empty
+        return true
+    return false
+
+def goal
+    if stack is full
+        return true
+    return false
+
+def current_position_same
+
+def current_position_different
+
+def pop
+
+def pull
+
+def recognition
+    compare fiducial
+    get id
+    get direction
